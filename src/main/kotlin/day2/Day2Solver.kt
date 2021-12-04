adfe7a47df4479a33dfcdb570b44e4b428658845
durgaprasadd <durgapda@thoughtworks.com>
2021-12-04 20:29:09 +0530

    refactor day1 solution using data classes

--- src/main/kotlin/day2/Day2Solver.kt
+++ src/main/kotlin/day2/Day2Solver.kt
@@ -2,36 +2,76 @@ package day2
 
 import model.DaySolver
 
+data class Command (
+    val direction: String,
+    val distance: Int
+    )
+
+data class Position (
+    private var horizontal: Int = 0,
+    private var depth: Int = 0,
+    private var aim:Int = 0
+        ) {
+    fun forward(distance: Int){
+        horizontal+= distance
+    }
+
+    fun forwardWithAim(distance: Int){
+        forward(distance)
+        depth += aim * distance
+    }
+
+    fun up(distance: Int){
+        depth -= distance
+    }
+
+    fun  down(distance: Int){
+        depth += distance
+    }
+
+    fun upAim(distance: Int){
+        aim -= distance
+    }
+
+    fun downAim(distance: Int){
+        aim += distance
+    }
+
+    fun product(): Int {
+        return horizontal * depth
+    }
+}
+
 class Day2Solver : DaySolver<List<String>, Int> {
-    override fun part1(data: List<String>): Int {
-        val result = data.map {
+    private fun parseData(data: List<String>): List<Command> {
+        return data.map {
             val x = it.split(' ')
-            x[0] to x[1].toInt()
+            Command(x[0], x[1].toInt())
         }
-        var point = Pair(0,0)
-        result.forEach {
-            when(it.first) {
-                "forward" -> point = Pair(point.first + it.second, point.second)
-                "up" -> point = Pair(point.first, point.second - it.second)
-                "down" -> point = Pair(point.first, point.second + it.second)
+    }
+    override fun part1(data: List<String>): Int {
+        val commands = parseData(data)
+        val position = Position()
+        commands.forEach {command ->
+            when(command.direction) {
+                "forward" -> position.forward(command.distance)
+                "up" -> position.up(command.distance)
+                "down" -> position.down(command.distance)
             }
         }
-        return point.first * point.second
+        return position.product()
     }
 
     override fun part2(data: List<String>): Int {
-        val result = data.map {
-            val x = it.split(' ')
-            x[0] to x[1].toInt()
-        }
-        var point = Triple(0,0, 0)
-        result.forEach {
-            when(it.first) {
-                "forward" -> point = Triple(point.first + it.second, point.second + (point.third * it.second), point.third)
-                "up" -> point = Triple(point.first, point.second, point.third - it.second)
-                "down" -> point = Triple(point.first, point.second, point.third + it.second)
+        val commands = parseData(data)
+        val position = Position()
+        commands.forEach {command ->
+            when(command.direction) {
+                "forward" -> position.forwardWithAim(command.distance)
+                "up" -> position.upAim(command.distance)
+                "down" -> position.downAim(command.distance)
             }
         }
-        return point.first * point.second
+        return position.product()
     }
 }
