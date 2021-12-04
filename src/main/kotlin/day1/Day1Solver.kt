53ea6681e7d8da4735a3e7dfbce1c423b6b581f7
durgaprasadd <durgapda@thoughtworks.com>
2021-12-04 20:04:53 +0530

    refactor day1 solution using windowed function

--- src/main/kotlin/day1/Day1Solver.kt
+++ src/main/kotlin/day1/Day1Solver.kt
@@ -4,24 +4,13 @@ import model.DaySolver
 
 class Day1Solver : DaySolver<List<Int>, Int> {
     override fun part1(data: List<Int>): Int {
-        var previous = 100000
-        var count = 0
-        data.forEach { x ->
-            if (x > previous) {
-                count++
-            }
-            previous = x
-        }
-        return count
+        return data.windowed(2)
+            .count {(x,y) -> y > x }
     }
 
     override fun part2(data: List<Int>): Int {
-        var count = 0
-        var index = 0
-        while (index < data.size - 3){
-            if (data[index] < data[index+3]) count++
-            index++
-        }
-        return count
+        return data.windowed(3)
+            .windowed(2)
+            .count {(x,y) -> y.sum() > x.sum() }
     }
 }
