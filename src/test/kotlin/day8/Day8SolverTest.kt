0d5519b194a5166c49482ca702e53613b2fcea56
durgaprasadd <durgapda@thoughtworks.com>
2021-12-09 10:28:40 +0530

    add day8 solution

---
+++ src/test/kotlin/day8/Day8SolverTest.kt
@@ -0,0 +1,34 @@
+package day8
+
+import helpers.readDataAsList
+import io.kotlintest.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class Day8SolverTest {
+    private val daySolver = Day8Solver()
+    private val dir = "day8"
+
+    @Test
+    fun `should return correct result for sample data for part1`() {
+        val data = readDataAsList(dir, "sample")
+        daySolver.part1(data) shouldBe 26
+    }
+
+    @Test
+    fun `should return correct result for original data for part1`() {
+        val data = readDataAsList(dir, "original")
+        daySolver.part1(data) shouldBe 504
+    }
+
+    @Test
+    fun `should return correct result for sample data for part2`() {
+        val data = readDataAsList(dir, "sample")
+        daySolver.part2(data) shouldBe 61229
+    }
+
+    @Test
+    fun `should return correct result for original data for part2`() {
+        val data = readDataAsList(dir, "original")
+        daySolver.part2(data) shouldBe 1073431
+    }
+}
