4aef734800540b828eea999075c6a141cee60fa6
durgaprasadd <durgapda@thoughtworks.com>
2022-12-13 07:19:29 +0530

    Add Day 37 (2022 - day 12)

--- src/test/kotlin/day25/Day31SolverTest.kt
+++ src/test/kotlin/day25/Day31SolverTest.kt
@@ -32,28 +32,4 @@ internal class Day31SolverTest {
         val data = readData(dir, "original")
         daySolver.part2(data) shouldBe 815
     }
-
-    @Test
-    fun something(){
-        val a = (15499).toDouble()/94744
-        for (i in 102557625 until 102557630){
-            val b = resilient(i)
-            println("$i $b $a ${b<a}")
-        }
-        println(resilient(102550000))
-    }
-
-    private fun resilient(n:Int): Double {
-        var count = 1.0
-        for (i in 2 until n){
-            if (gcd(n,i) == 1){
-                count++
-            }
-        }
-        return count/(n-1)
-    }
-
-    private fun gcd(a:Int,b:Int): Int {
-        return if (a%b == 0) b else gcd(b, a%b)
-    }
 }
