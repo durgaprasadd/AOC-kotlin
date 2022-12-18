0510912850d575c1d39a6e7ce670aff7f2e422ec
durgaprasadd <durgapda@thoughtworks.com>
2022-12-18 15:55:28 +0530

    Add Day 41 (2022 - day 16)

---
+++ src/test/kotlin/day25/Day41SolverTest.kt
@@ -0,0 +1,34 @@
+package day25
+
+import helpers.readDataAsList
+import io.kotlintest.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class Day41SolverTest {
+    private val daySolver = Day41Solver()
+    private val dir = "day41"
+
+    @Test
+    fun `should return correct result for sample data for part1`() {
+        val data = readDataAsList(dir, "sample")
+        daySolver.part1(data) shouldBe 1651
+    }
+
+    @Test
+    fun `should return correct result for original data for part1`() {
+        val data = readDataAsList(dir, "original")
+        daySolver.part1(data) shouldBe 1857
+    }
+
+    @Test
+    fun `should return correct result for sample data for part2`() {
+        val data = readDataAsList(dir, "sample")
+        daySolver.part2(data) shouldBe 1707
+    }
+
+    @Test
+    fun `should return correct result for original data for part2`() {
+        val data = readDataAsList(dir, "original")
+        daySolver.part2(data) shouldBe 2536
+    }
+}
